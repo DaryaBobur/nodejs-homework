253fb966544acc174032dc27e960949f8d32a591
Darya <daryabobur@gmail.com>
2022-12-03 13:29:34 +0200

    Update app.js

--- schemas/contact.js
+++
@@ -1,9 +0,0 @@
-const Joi = require('joi');
-
-const contactsSchema = Joi.object({
-  name: Joi.string().required(),
-  phone: Joi.string().min(10).max(10).required(),
-  email: Joi.string().email().required(),
-});
-
-module.exports = contactsSchema;
