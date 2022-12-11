29a1a366710ffe6db85833c910daea14cf876240
Darya <daryabobur@gmail.com>
2022-12-11 16:35:13 +0200

    Create users.js

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { users: ctrl } = require("../../controllers");
+
+router.post('/register', ctrl.register);
+router.get('/login', ctrl.login);
+
+
+
+module.exports = router;
