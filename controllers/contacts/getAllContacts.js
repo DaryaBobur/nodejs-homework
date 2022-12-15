55fd47f7f88765236aa7aa1077a2f3ae1e7a54f7
Darya <daryabobur@gmail.com>
2022-12-15 10:59:12 +0200

    Update getAllContacts.js

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,7 +3,24 @@ const { Contact } = require('../../models/contact');
 const listContacts = async (req, res, next) => {
   try {
     const {_id} = req.user;
-    const contacts = await Contact.find({owner: _id});
+    const {page = 1, limit = 20, favorite} = req.query;
+    const skip = (page - 1) * limit; 
+
+    const contacts = await Contact.find({owner: _id},  "", {skip, limit: Number(limit)}).populate("owner", "_id email subscription");
+    
+    if(req.query.favorite) {
+      const favoriteContacts = await Contact.find({owner: _id, favorite},  "", {skip, limit: Number(limit)})
+      .populate("owner", "_id email subscription");
+
+      return res.json({
+        status: 'success',
+        code: 200,
+        data: {
+          result: favoriteContacts,
+        },
+      });
+    };
+    
     res.json({
       status: 'success',
       code: 200,
