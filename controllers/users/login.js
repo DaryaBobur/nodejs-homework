4b8afff76b4bc89ddc9b58776ed981d922b61e19
Darya <daryabobur@gmail.com>
2022-12-15 10:59:25 +0200

    Update login.js

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,42 +1,38 @@
 const { User, joiLoginSchema } = require('../../models/user');
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = process.env;
 
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const {error} = joiLoginSchema.validate(req.body);
-    
+    const { error } = joiLoginSchema.validate(req.body);
+
     if (error) {
       error.status = 400;
       throw error;
     }
-    
     const user = await User.findOne({ email });
-// error
-    const passCompare = bcrypt.compareSync(password, user.password);
-    if(!user || !passCompare) {
-    const error = new Error(`Email or password is wrong`);
+    if (!user || !user.comparePassword(password)) {
+      const error = new Error(`Email or password is wrong`);
       error.status = 401;
       throw error;
     }
 
     const payload = {
-        id: user._id
+      id: user._id,
     };
-    const {subscription} = user;
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "1w"});
-    await User.findByIdAndUpdate(user._id, {token});
+    const { subscription } = user;
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1d' });
+    await User.findByIdAndUpdate(user._id, { token });
     res.json({
-        status: "success",
-        code: 200,
-        token: token,
-        user: {
-            email,
-            subscription
-        }
-    })
+      status: 'success',
+      code: 200,
+      token: token,
+      user: {
+        email,
+        subscription,
+      },
+    });
   } catch (error) {
     next(error);
   }
