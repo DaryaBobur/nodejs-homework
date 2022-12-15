79e2641d0cc7a86b120043bd671d407eee6e9c09
Darya <daryabobur@gmail.com>
2022-12-15 10:59:47 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { users: ctrl } = require("../../controllers");
+const {user} = require('../../middlewares');
 
 router.post('/register', ctrl.register);
+
 router.get('/login', ctrl.login);
 
+router.post('/logout', user, ctrl.logout);
 
+router.get('/current', user, ctrl.currentUser);
+
+router.patch('/:contactId/users', ctrl.updateSubscriptionUser);
 
 module.exports = router;
